a28821a2439440711e19b5163d401eed73764965
Dhayv <davidhyppolitedh@gmail.com>
2024-07-15 16:22:12 -0400

    permission to view/tested endpoints

--- backend/app/recipes/views.py
+++ backend/app/recipes/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 import requests
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .permissions import ReadOnlyPermissions
 
 
 # Create your views here.
 # fetch and return categories.
 class CategoryList(APIView):
+    permission_classes = [ReadOnlyPermissions]
+
     def get(self, request):
         response = requests.get('https://www.themealdb.com/api/json/v1/1/categories.php')
         data = response.json()
@@ -15,6 +18,8 @@ class CategoryList(APIView):
 
 # fetch and return recipes by categories.
 class RecipeList(APIView):
+    permission_classes = [ReadOnlyPermissions]
+
     def get(self, request):
         category = request.query_params.get('category')
         if not category:
@@ -26,6 +31,8 @@ class RecipeList(APIView):
 
 # fetch and return recipe details information.
 class RecipeDetail(APIView):
+    permission_classes = [ReadOnlyPermissions]
+
     def get(self, request, recipe_id):
         response = requests.get(f'https://www.themealdb.com/api/json/v1/1/lookup.php?i={recipe_id}')
         data = response.json()
