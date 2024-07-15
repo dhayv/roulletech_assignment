02d2dde00a81fce9cad5dde96c8252c3b24aff60
Dhayv <davidhyppolitedh@gmail.com>
2024-07-14 21:23:23 -0400

    code: views to fetch use rest_framework

--- backend/app/recipes/views.py
+++ backend/app/recipes/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+import requests
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 # Create your views here.
+class CategoryList(APIView):
+    def get(self, request):
+        response = requests.get('https://www.themealdb.com/api/json/v1/1/categories.php')
+        data = response.json()
+        return Response(data)
+
+
+class RecipeList(APIView):
+    def get(self, request):
+        category = request.query_params.get('category')
+        if not category:
+            return Response({'error': "No category found"}, status="404")
+        response = requests.get(f'https://www.themealdb.com/api/json/v1/1/filter.php?c={category}')
+        data = response.json()
+        return Response(data)
+
+
+class RecipeLDetail(APIView):
+    def get(self, request, recipe_id):
+        response = requests.get(f'https://www.themealdb.com/api/json/v1/1/lookup.php?i={recipe_id}')
+        data = response.json()
+        return Response(data)
