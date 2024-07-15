2bd01ad1be8bb72f0e0c890cdb211da341a29d8e
Dhayv <davidhyppolitedh@gmail.com>
2024-07-14 20:30:10 -0400

    added rest_framework

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "recipes"
+    "rest_framework",
+    "recipes",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
