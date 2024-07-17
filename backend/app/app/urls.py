7586f61dbf7d0cbedc7bb1ebf4b3638c8ae43686
Dhayv <davidhyppolitedh@gmail.com>
2024-07-17 16:58:25 -0400

    removed extra recipes in domain

--- backend/app/app/urls.py
+++ backend/app/app/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/recipes/", include("recipes.urls")),
+    path("api/", include("recipes.urls")),
 ]
